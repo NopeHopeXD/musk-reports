db51d45d8d88fd926b95c18968e57305aa1c6a02
NotABot <iamnotabot.xx@gmail.com>
2021-10-29 13:41:04 +0100

    Updated my form to the current version after lots of pain

---
+++ musk reports/Reports.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace musk_reports
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.Grid = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.Menu = new System.Windows.Forms.Panel();
+            this.Test = new System.Windows.Forms.Button();
+            this.TitleBox = new System.Windows.Forms.Panel();
+            this.Label = new System.Windows.Forms.Label();
+            this.TestDisp = new System.Windows.Forms.Label();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Grid)).BeginInit();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.Menu.SuspendLayout();
+            this.TitleBox.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.Grid);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Location = new System.Drawing.Point(196, 58);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(799, 453);
+            this.panel2.TabIndex = 3;
+            // 
+            // Grid
+            // 
+            this.Grid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.Grid.Location = new System.Drawing.Point(9, 10);
+            this.Grid.Name = "Grid";
+            this.Grid.Size = new System.Drawing.Size(787, 440);
+            this.Grid.TabIndex = 2;
+            this.Grid.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvPerson_CellContentClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Visible = false;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 19.35807F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 80.64192F));
+            this.tableLayoutPanel1.Controls.Add(this.Menu, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.TitleBox, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.panel2, 1, 1);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(12, 12);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10.70039F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 89.29961F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1001, 517);
+            this.tableLayoutPanel1.TabIndex = 1;
+            // 
+            // Menu
+            // 
+            this.Menu.Controls.Add(this.TestDisp);
+            this.Menu.Controls.Add(this.Test);
+            this.Menu.Location = new System.Drawing.Point(3, 58);
+            this.Menu.Name = "Menu";
+            this.Menu.Size = new System.Drawing.Size(186, 453);
+            this.Menu.TabIndex = 1;
+            // 
+            // Test
+            // 
+            this.Test.Location = new System.Drawing.Point(31, 19);
+            this.Test.Name = "Test";
+            this.Test.Size = new System.Drawing.Size(75, 23);
+            this.Test.TabIndex = 0;
+            this.Test.Text = "Load Report";
+            this.Test.UseVisualStyleBackColor = true;
+            this.Test.Click += new System.EventHandler(this.Test_Click);
+            // 
+            // TitleBox
+            // 
+            this.TitleBox.Controls.Add(this.Label);
+            this.TitleBox.Location = new System.Drawing.Point(3, 3);
+            this.TitleBox.Name = "TitleBox";
+            this.TitleBox.Size = new System.Drawing.Size(186, 48);
+            this.TitleBox.TabIndex = 2;
+            // 
+            // Label
+            // 
+            this.Label.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.Label.AutoSize = true;
+            this.Label.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Label.Location = new System.Drawing.Point(26, 12);
+            this.Label.Name = "Label";
+            this.Label.Size = new System.Drawing.Size(132, 25);
+            this.Label.TabIndex = 0;
+            this.Label.Text = "Musk Reports";
+            this.Label.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // TestDisp
+            // 
+            this.TestDisp.AutoSize = true;
+            this.TestDisp.Location = new System.Drawing.Point(55, 91);
+            this.TestDisp.Name = "TestDisp";
+            this.TestDisp.Size = new System.Drawing.Size(34, 13);
+            this.TestDisp.TabIndex = 1;
+            this.TestDisp.Text = "Temp";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1025, 541);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Grid)).EndInit();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.Menu.ResumeLayout(false);
+            this.Menu.PerformLayout();
+            this.TitleBox.ResumeLayout(false);
+            this.TitleBox.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Panel Menu;
+        private System.Windows.Forms.Button Test;
+        private System.Windows.Forms.Panel TitleBox;
+        private System.Windows.Forms.Label Label;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView Grid;
+        private System.Windows.Forms.Label TestDisp;
+    }
+}
---
+++ musk reports/Reports.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace musk_reports
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dtSetup(DataTable dt)
+        {
+            dt.Columns.Add("Subsections", typeof(string));
+            dt.Columns.Add("T&L", typeof(int));
+            dt.Columns.Add("AkzoNobel Snowmarket", typeof(int));
+            dt.Columns.Add("Weetabix", typeof(int));
+            dt.Columns.Add("Koito", typeof(int));
+            dt.Columns.Add("Mars Melton", typeof(int));
+            dt.Columns.Add("Projects", typeof(int));
+            dt.Columns.Add("Design", typeof(int));
+            dt.Columns.Add("Misc", typeof(int));
+            dt.Columns.Add("Monthly Total", typeof(int));
+
+            
+        }
+
+        private void doSQLshit()
+        {
+            //fuck sql just sayin XD
+            
+            DatabaseConnection dbConn = new DatabaseConnection();
+            string TestText = "";
+
+            DataTable dt = new DataTable();
+            dbConn.getData("");
+            Grid.DataSource = dt;
+            //set up table for data
+            dtSetup(dt);
+
+            foreach (var d in dbConn.data)
+            {
+                TestText += d.Comment + ", ";
+                
+            }
+            TestDisp.Text = TestText;
+        }
+
+        private void Test_Click(object sender, EventArgs e)
+        {
+            Test.Text = "Loading DB";
+
+            doSQLshit();
+
+            //Test.Text = "Something Happened";
+
+        }
+
+        private void DispData_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dgvPerson_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
