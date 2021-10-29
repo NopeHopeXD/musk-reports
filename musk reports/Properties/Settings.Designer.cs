49db6cda897591499e30e5d22f13c6f448171f45
NotABot <iamnotabot.xx@gmail.com>
2021-10-29 13:22:24 +0100

    added dbConnection class

--- musk reports/Properties/Settings.Designer.cs
+++ musk reports/Properties/Settings.Designer.cs
@@ -8,22 +8,30 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace musk_reports.Properties
-{
-
-
+namespace musk_reports.Properties {
+    
+    
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "11.0.0.0")]
-    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase
-    {
-
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.1.0.0")]
+    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
+        
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
-
-        public static Settings Default
-        {
-            get
-            {
+        
+        public static Settings Default {
+            get {
                 return defaultInstance;
+            }
+        }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=\"H:\\My Documents\\Development\\" +
+            "Oct29\\musk-reports\\musk reports\\bin\\Debug\\Muskdata.mdf\";Integrated Security=True" +
+            ";Connect Timeout=30")]
+        public string DBConnStr {
+            get {
+                return ((string)(this["DBConnStr"]));
             }
         }
     }
---
+++ musk reports/dbConnection.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace musk_reports
+{
+    class DatabaseConnection
+
+
+    {
+        //
+        //attrs
+        //
+        
+        private static DatabaseConnection _instance;
+        private static string connStr;
+
+        //SQL conn obj to stor db connection
+        private SqlConnection connToDB;
+
+        public List<Data> data = new List<Data>();
+
+        //constructors
+
+        public DatabaseConnection(){
+            connStr = Properties.Settings.Default.DBConnStr;
+        }
+
+
+
+        //Methods
+        /*
+        public static DatabaseConnection getDBconn() {
+            if (_instance == null) {
+                _instance = new DatabaseConnection();
+            }
+            return _instance;
+        }
+        
+        public DataSet GetDataSet(string sqlComand) {
+            DataSet dataset = new DataSet();
+
+            using (connToDB = new SqlConnection(connStr))
+            {
+                connToDB.Open();
+                SqlDataAdapter dataAdapter = new SqlDataAdapter(sqlComand, connToDB);
+
+                dataAdapter.Fill(dataset);
+
+                connToDB.Close();
+            }
+
+            return dataset;
+        }
+        */
+
+        public void getData(string company)
+        {
+            /*
+
+            List<Data> data = new List<Data>();
+
+            HeaderIDs = FROM Header SELECT HeaderID WHERE 'Work Area# == company
+            for j in HeaderIDs
+                DataSets = FROM DataSet SELECT DataSetID WHERE HeaderID = j
+                for k in DataSets
+                    Data d = new Data();
+                    d.add...
+                    ...
+                    data.add(d);
+            return data
+             
+             */
+            
+            using (SqlConnection cn = new SqlConnection(connStr))
+            {
+                cn.Open();
+
+                SqlCommand sqlCommand = new SqlCommand("SELECT * FROM Data Where DataSetID = 1", cn);
+                SqlDataReader reader = sqlCommand.ExecuteReader();
+                while (reader.Read())
+                {
+                    Data d = new Data();
+                    d.DataID = (int)reader["DataID"];
+                    d.DataSetID = (int)reader["DataSetID"];
+                    d.InterventionNo = (int)reader["InterventionNo"];
+                    d.Interventions = (int)reader["Interventions"];
+                    d.Comment = (string)reader["Comment"];
+                    d.ActionTaken = (string)reader["ActionTaken"];
+                    d.Positive = (bool)reader["Positive"];
+                    d.Completed = (bool)reader["Completed"];
+                    data.Add(d);
+                }
+                cn.Close();
+            }
+            
+        }
+    }
+
+    class Data
+    {
+        //
+        //attrs
+        //
+
+        public int DataID;
+        public int DataSetID;
+        public int InterventionNo;
+        public int Interventions;
+        public string Comment;
+        public string ActionTaken;
+        public bool Positive;
+        public bool Completed;
+
+    }
+}
