03728ec34d6bb0e3d98f5972fe4bacf64c58e5a9
lloydalexporter <porterlloydemail@gmail.com>
2021-11-05 13:18:53 +0000

    Few cheeky changes

--- musk reports/Properties/Settings.Designer.cs
+++ musk reports/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace musk_reports.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.1.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.10.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -26,9 +26,9 @@ namespace musk_reports.Properties {
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
-        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=\"H:\\My Documents\\Development\\" +
-            "Oct29\\musk-reports\\musk reports\\bin\\Debug\\Muskdata.mdf\";Integrated Security=True" +
-            ";Connect Timeout=30")]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=\"C:\\Users\\Lloyd Alex Porter\\D" +
+            "esktop\\musk-reports\\musk reports\\Muskdata.mdf\";Integrated Security=True;Connect " +
+            "Timeout=30")]
         public string DBConnStr {
             get {
                 return ((string)(this["DBConnStr"]));
