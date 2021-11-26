619eb9104a6be01100649709980502060497a681
NotABot <iamnotabot.xx@gmail.com>
2021-11-26 14:23:15 +0000

    more fixes

--- musk reports/YTD report.Designer.cs
+++ musk reports/YTD report.Designer.cs
@@ -187,13 +187,6 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(96, 23);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(90, 23);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "Delete Report";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // panel1
             // 
