059f6e59a68d3b4f63cb472fde453f733a1a2342
Adam Antczak <AA2383@student.aru.ac.uk>
2021-12-03 12:56:59 +0000

    Minor changes

--- musk reports/DatabaseConnection.cs
+++ musk reports/DatabaseConnection.cs
@@ -9,12 +9,8 @@ using System.Data.SqlClient;
 namespace musk_reports
 {
     class DatabaseConnection
-
-
     {
-        //
-        //attrs
-        //
+        /* Attributes - - - - - - - - - - - - - - - - - - - - */
         
         private static DatabaseConnection _instance;
         private static string connStr;
@@ -22,41 +18,20 @@ namespace musk_reports
         //SQL conn obj to stor db connection
         private SqlConnection connToDB;
 
+        // Create List from 
         public List<Data> data = new List<Data>();
 
         //constructors
 
-        public DatabaseConnection(){
+        public DatabaseConnection() {
             connStr = Properties.Settings.Default.DBConnStr;
         }
 
 
 
-        //Methods
-        /*
-        public static DatabaseConnection getDBconn() {
-            if (_instance == null) {
-                _instance = new DatabaseConnection();
-            }
-            return _instance;
-        }
-        
-        public DataSet GetDataSet(string sqlComand) {
-            DataSet dataset = new DataSet();
-
-            using (connToDB = new SqlConnection(connStr))
-            {
-                connToDB.Open();
-                SqlDataAdapter dataAdapter = new SqlDataAdapter(sqlComand, connToDB);
-
-                dataAdapter.Fill(dataset);
-
-                connToDB.Close();
-            }
-
-            return dataset;
-        }
-        */
+        /* Methods  - - - - - - - - - - - - - - - - - - - - - */
+         
+        // ! - Get Data from the Database - NOT USED
         public DataTable dtGet(string Command)
         {
             using (SqlConnection con = new SqlConnection(connStr))
@@ -70,7 +45,7 @@ namespace musk_reports
             }
         }
 
-
+        // ! - Get Data from the Database - NOT USED
         public List<Data> generalGetData(string command, params string[] attrs) {
 
             List<Data> d = new List<Data>();
@@ -95,15 +70,22 @@ namespace musk_reports
 
         }
 
+
+        // ! - Get Data from the Database
         public void getData(string SQLCommand)
         {
+            // Create new SQL Connection
             using (SqlConnection cn = new SqlConnection(connStr))
             {
+                // Connect to Database
                 cn.Open();
 
+                // Run the SQL Command...
                 SqlCommand sqlCommand = new SqlCommand(SQLCommand, cn);
                 SqlDataReader reader = sqlCommand.ExecuteReader();
-                while (reader.Read())
+                
+                // ... and read through each line
+                while (reader.Read()) 
                 {
                     Data d = new Data();
                     d.DataID = (int)reader["DataID"];
@@ -114,48 +96,73 @@ namespace musk_reports
                     d.ActionTaken = (string)reader["ActionTaken"];
                     d.Positive = (bool)reader["Positive"];
                     d.Completed = (bool)reader["Completed"];
+                    
+                    // Add Data (d) to the Data List
                     data.Add(d);
                 }
+
+                // Close the Connection
                 cn.Close();
             }
             
         }
 
-        public void RemoveReportData(int tempReportID)
+
+        // ! - Remove the Report Data
+        public void RemoveReportData(string tempReportID)
         {
+            // Create new SQL Connection
             using (SqlConnection conn = new SqlConnection(connStr))
             {
                 // The reportID will be received from the form once the user selects it
                 string tempSql = "DELETE FROM report WHERE reportID = @reportID";
                 SqlCommand tempSqlCommand = new SqlCommand(tempSql, conn);
-                // IMPORTANT - Adding the parameters like this helps against SQL injections
+
+                // Adding the parameters manually helps against SQL injections
                 tempSqlCommand.Parameters.AddWithValue("@reportID", tempReportID);
 
+                // Connect to Database
                 conn.Open();
 
+
                 try {
+                    // Try to Execute the Command
                     int temp = tempSqlCommand.ExecuteNonQuery();
                 }
-                catch(Exception error) {
+                catch (Exception error) {
+                    // Failing shows an error
                     Console.WriteLine(error.Message);
                 }
                 
+                // Close the Connection
                 conn.Close();
             }
         }
 
+
+        // ! - Get Data from the Report
         public List<Report> GetReportData()
         {
+            // Create List to put Data
             List<Report> tempReportList = new List<Report>();
+            
+            // Create new SQL Connection
             using (SqlConnection conn = new SqlConnection(connStr))
             {
+                // Prepare the SQL Command
                 string tempSql = "SELECT * FROM report";
                 SqlCommand tempSqlCommand = new SqlCommand(tempSql, conn);
+
+                // Connect to Database
                 conn.Open();
+
+                // Try to...
                 try
                 {
+                    // ... execute the SQL command...
                     using (SqlDataReader tempReader = tempSqlCommand.ExecuteReader())
                     {
+                        // ... and read through each line
                         while(tempReader.Read())
                         {
                             Report tempReport = new Report();
@@ -169,14 +176,18 @@ namespace musk_reports
                 }
                 catch(Exception error)
                 {
+                    // Else say error.
                     Console.WriteLine(error.Message);
                 }
 
+                // Close connection
                 conn.Close();
             }
 
+            // Return the List containing the Report's Data
             return tempReportList;
         }
+    }
 
         public string GetStaffNameFromID(int staffID)
         {
@@ -207,32 +218,9 @@ namespace musk_reports
                 conn.Close();
             }
 
-            return temp;
-        }
-
-        // Saving the data from the 'AddReport.cs' or 'AddNewReport.cs' - idk which one we're using
-        public void dataTableToDatabase()
-        {
-            using (SqlConnection conn = new SqlConnection(connStr))
-            {
-                conn.Open();
-                // I tried lots of stuff here, couldn't work it out
-
-                /** Adam - it's nothing too problematic, just the conn variable missing the actual connection (connStr), should work fine now
-                 *  As for the actual saving to the database, the general format should be the same as for the data read functions
-                 *  Instead of the reader, the SqlCommand needs to be prepared, connection opened and then the query executed using something like .ExecuteNonQuery()
-                 *  You can look at RemoveReportData() as an example, though warning I haven't tested it yet lol
-                 **/
-                conn.Close();
-            }
-        }
-    }
-
-    class Data
+class Data
     {
-        //
-        //attrs
-        //
+        /* Attributes - - - - - - - - - - - - - - - - - - - - */
 
         public int DataID;
         public int DataSetID;
@@ -244,40 +232,130 @@ namespace musk_reports
         public bool Completed;
 
 
+        /* Methods  - - - - - - - - - - - - - - - - - - - - - */
+
+        // Set Variables for Data
         public void setVars(string str, string data)
         {
-            if (str == "DataID")
+
+            // Switch-Case for setting up variables
+            switch (str)
             {
-                this.DataID = Int16.Parse(data);
-            }
-            else if (str == "DataSetID")
-            {
-                this.DataSetID = Int16.Parse(data);
-            }
-            else if (str == "InterventionNo")
-            {
-                this.InterventionNo = Int16.Parse(data);
-            }
-            else if (str == "Interventions")
-            {
-                this.Interventions = Int16.Parse(data);
-            }
-            else if (str == "Comment")
-            {
-                this.Comment = data;
-            }
-            else if (str == "ActionTaken")
-            {
-                this.ActionTaken = data;
+                case "DataID":
+                    this.DataID = Int16.Parse(data);
+                    break;
+                case "DataSetID":
+                    this.DataSetID = Int16.Parse(data);
+                    break;
+                case "InterventionNo":
+                    this.InterventionNo = Int16.Parse(data);
+                    break;
+                case "Interventions":
+                    this.Interventions = Int16.Parse(data);
+                    break;
+                case "Comment": 
+                    this.Comment = data;
+                    break;
+                case "ActionTaken":
+                    this.ActionTaken = data;
+                    break;
             }
         }
     }
 
     class Report
     {
+        
+        /* Attributes - - - - - - - - - - - - - - - - - - - - */
+
         public int ReportID;
         public int StaffID;
         public int HeaderID;
         public int DataSetID;
     }
+
+    class Header
+    {
+        /* Attributes - - - - - - - - - - - - - - - - - - - - */
+
+
+        public int HeaderID;
+        public string Site;
+        public string WorkArea;
+        public string Supervisor;
+        public string JobDesc;
+        public DateTime Date;
+        public string Type;
+
+
+        /* Methods  - - - - - - - - - - - - - - - - - - - - - */
+
+        // Set Variables for Header
+        public void setVars(string str, string header)
+        {
+
+            // Switch-Case for setting up variables
+            switch (str)
+            {
+                case "HeaderID":
+                    this.HeaderID = Int16.Parse(header);
+                    break;
+                case "Comment":
+                    this.Site = header;
+                    break;
+                /*case "ActionTaken":
+                    this.ActionTaken = header;
+                    break;*/
+            }
+        }
+    }
 }
+
+
+
+// ! - Notes / Code Dump:
+/*
+ 
+
+public static DatabaseConnection getDBconn() {
+    if (_instance == null) {
+        _instance = new DatabaseConnection();
+    }
+        return _instance;
+}
+        
+public DataSet GetDataSet(string sqlComand) {
+    DataSet dataset = new DataSet();
+
+    using (connToDB = new SqlConnection(connStr))
+    {
+        connToDB.Open();
+        SqlDataAdapter dataAdapter = new SqlDataAdapter(sqlComand, connToDB);
+
+        dataAdapter.Fill(dataset);
+
+        connToDB.Close();
+    }
+    return dataset;
+}
+
+
+
+ // ! - Saving the data from the 'AddReport.cs' or 'AddNewReport.cs' - idk which one we're using
+public void dataTableToDatabase() {
+    
+    using (SqlConnection conn = new SqlConnection(connStr))
+    {
+        conn.Open();
+        // I tried lots of stuff here, couldn't work it out
+
+        // Adam - it's nothing too problematic, just the conn variable missing the actual connection (connStr), should work fine now
+        // As for the actual saving to the database, the general format should be the same as for the data read functions
+        //  Instead of the reader, the SqlCommand needs to be prepared, connection opened and then the query executed using something like .ExecuteNonQuery()
+        //  You can look at RemoveReportData() as an example, though warning I haven't tested it yet lol
+                 
+        conn.Close();
+    }
+}
+
+   
--- musk reports/addReport.cs
+++ musk reports/addReport.cs
@@ -8,7 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace createR
+namespace musk_reports
 {
     public partial class addReportF : Form
     {
