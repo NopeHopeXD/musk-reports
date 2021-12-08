1e6ddcca3936783da12a605ae7303c9aa5dd9e67
lloydalexporter <porterlloydemail@gmail.com>
2021-12-08 21:32:32 +0000

    Commit Number 100! Yay! Ohh and just added the Icon to the new windows.

--- musk reports/LoginForm.Designer.cs
+++ musk reports/LoginForm.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Login));
             this.loginId = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
@@ -43,7 +44,7 @@
             this.loginId.Location = new System.Drawing.Point(85, 72);
             this.loginId.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.loginId.Name = "loginId";
-            this.loginId.Size = new System.Drawing.Size(68, 22);
+            this.loginId.Size = new System.Drawing.Size(110, 33);
             this.loginId.TabIndex = 0;
             this.loginId.Text = "LoginId";
             // 
@@ -53,31 +54,31 @@
             this.label2.Location = new System.Drawing.Point(65, 123);
             this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(89, 22);
+            this.label2.Size = new System.Drawing.Size(143, 33);
             this.label2.TabIndex = 1;
             this.label2.Text = "Password";
             // 
             // textBox1
             // 
             this.textBox1.Location = new System.Drawing.Point(156, 68);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(163, 28);
+            this.textBox1.Size = new System.Drawing.Size(163, 40);
             this.textBox1.TabIndex = 3;
             // 
             // textBox2
             // 
             this.textBox2.Location = new System.Drawing.Point(156, 123);
-            this.textBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(163, 28);
+            this.textBox2.Size = new System.Drawing.Size(163, 40);
             this.textBox2.TabIndex = 4;
             this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
             // 
             // button1
             // 
             this.button1.Location = new System.Drawing.Point(86, 185);
-            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(105, 45);
             this.button1.TabIndex = 5;
@@ -88,7 +89,7 @@
             // button2
             // 
             this.button2.Location = new System.Drawing.Point(244, 185);
-            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button2.Margin = new System.Windows.Forms.Padding(4);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(105, 45);
             this.button2.TabIndex = 6;
@@ -103,14 +104,16 @@
             this.label1.Location = new System.Drawing.Point(25, 19);
             this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(387, 22);
+            this.label1.Size = new System.Drawing.Size(604, 33);
             this.label1.TabIndex = 7;
             this.label1.Text = "Welcome to the Musk Reports Application.";
             // 
             // Login
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 22F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(17F, 33F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
             this.ClientSize = new System.Drawing.Size(425, 252);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button2);
@@ -121,7 +124,8 @@
             this.Controls.Add(this.loginId);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "Login";
